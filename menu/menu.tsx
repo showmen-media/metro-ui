86a141e2ee13c7cf5cc3c233398fa79fa57bd65a
Philip Psychias <pjs@fips.gr>
2025-10-27 21:45:20 +0200

    Type fixes

--- menu/menu.tsx
+++ menu/menu.tsx
@@ -1,6 +1,12 @@
+import { ReactNode } from "react";
 
-export default function Menu({ className, children }) {
+export default function Menu({ className, children }: MenuProps) {
 	return <div className={["flex flex-col", className].filter(Boolean).join(' ')}>
 		{children}
 	</div>;
+}
+
+type MenuProps = {
+	className?: string;
+	children?: ReactNode;
 }
